e495f3ea46438c07a16b0ae0b7db76e9e89b001a
Mumtaz dev <162595452+uzmumtazdev@users.noreply.github.com>
2024-07-13 12:20:24 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
